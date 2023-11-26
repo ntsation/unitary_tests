fd8dee960e9089b90d18e6e062f69cf86dc463f5
Nathan Souza <naathanmarinho11@gmail.com>
2023-11-25 22:32:24 -0300

    adds new tests

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,18 +1,35 @@
 import unittest
-from main import calcular_media
+from main import calculate_average
 
-class TestCalculadora(unittest.TestCase):
 
-   def test_media(self):
-      self.assertEqual(calcular_media([10, 20, 30, 40, 50]), 30)
+class TestCalculator(unittest.TestCase):
 
-   def test_lista_vazia(self):
-      with self.assertRaises(ValueError):
-         calcular_media([])
-   
-   def test_media_negativos(self):
-      self.assertEqual(calcular_media([-10, -20, -30, -40, -50]), -30)
+    def test_media_lista_positivos(self):
+        self.assertEqual(calculate_average
+        ([10, 20, 30, 40, 50]), 30)
+
+    def test_media_lista_negativos(self):
+        self.assertEqual(calculate_average
+        ([-10, -20, -30, -40, -50]), -30)
+
+    def test_media_mista(self):
+        self.assertEqual(calculate_average
+        ([-10, 20, -30, 40, -50]), -6)
+
+    def test_media_decimal(self):
+        self.assertAlmostEqual(calculate_average([1.5, 2.5, 3.5, 4.5, 5.5]), 3.5, places=2)
+
+    def test_empty_list(self):
+        with self.assertRaises(ValueError):
+            calculate_average([])
+
+    def test_lista_nao_numerica(self):
+        with self.assertRaises(TypeError):
+            calculate_average
+            (["a", "b", "c"])
+
+    def test_media_one_element(self):
+        self.assertEqual(calculate_average([42]), 42)
 
 if __name__ == "__main__":
-   unittest.main()
-   
+    unittest.main()
